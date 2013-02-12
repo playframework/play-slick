ec2105aa63445bcbf80768ee48d212cbecd3f884
Fredrik Ekholdt <fredrik.ekholdt@typesafe.com>
2013-02-12 13:02:05 +0100

    added sample

---
+++ samples/play-slick-sample/app/controllers/Application.scala
@@ -0,0 +1,37 @@
+package controllers
+
+import models._
+import play.api._
+import play.api.db.slick.Config.driver.simple._
+import play.api.db.slick.DB
+import play.api.data._
+import play.api.data.Forms._
+import play.api.mvc._
+
+object Application extends Controller {
+  
+  import play.api.Play.current
+
+  def index = Action {
+    DB.withSession{ implicit session =>
+      Ok(views.html.index(Query(Cats).list))
+    }
+  }
+
+  val catForm = Form(
+    mapping(
+      "name" -> text(),
+      "color" -> text()
+    )(Cat.apply)(Cat.unapply)
+  )
+  
+  def insert = Action { implicit request =>
+    val cat = catForm.bindFromRequest.get
+    DB.withSession{ implicit session =>
+      Cats.insert(cat)
+    }
+
+    Redirect(routes.Application.index)
+  }
+  
+}
---
+++ samples/play-slick-sample/app/models/Cat.scala
@@ -0,0 +1,13 @@
+package models
+
+import play.api.db.slick.Config.driver._
+
+case class Cat(name: String, color: String)
+
+object Cats extends Table[Cat]("CAT") {
+
+  def name = column[String]("name", O.PrimaryKey)
+  def color = column[String]("color", O.NotNull)
+
+  def * = name ~ color <> (Cat.apply _, Cat.unapply _)
+}
