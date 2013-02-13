b70747367f7171b13465ac652a4b7c5e237d8238
Loic Descotte <loic.descotte@gmail.com>
2013-02-13 11:06:37 +0100

    better error management

--- src/main/scala/play/api/db/slick/Config.scala
+++ src/main/scala/play/api/db/slick/Config.scala
@@ -5,7 +5,8 @@ import play.api.Play.current
 import scala.slick.driver._
 
 object Config {
-  lazy val driver: ExtendedDriver = Play.application.configuration.getString(driverConfiguration).get match {
+  val conf = Play.application.configuration
+  lazy val driver: ExtendedDriver = driverConfiguration(conf) match {
     case "org.apache.derby.jdbc.EmbeddedDriver" => DerbyDriver
     case "org.h2.Driver" => H2Driver    
     case "org.hsqldb.jdbcDriver" => HsqldbDriver
@@ -13,15 +14,18 @@ object Config {
     case "org.postgresql.Driver" => PostgresDriver
     case "org.sqlite.JDBC" => SQLiteDriver
     case "com.microsoft.sqlserver.jdbc.SQLServerDriver" => SQLServerDriver
-    case _ => throw new RuntimeException("Slick error : Unknown jdbc driver found in application.conf")
+    case _ => throw conf.reportError("slick jdbc driver", "Slick error : Unknown jdbc driver found in application.conf: [" + driver + "]", None)
   }
 
-  def driverConfiguration = {
-    Play.application.mode match {     
-      case Mode.Test if(Play.application.configuration.getString("test.db.default.driver").isDefined) => "test.db.default.driver"
-      case Mode.Prod if(Play.application.configuration.getString("prod.db.default.driver").isDefined) => "prod.db.default.driver"
+  def driverConfiguration(conf: Configuration) = {
+    val driverConfKey = Play.application.mode match {     
+      case Mode.Test if(conf.getString("test.db.default.driver").isDefined) => "test.db.default.driver"
+      case Mode.Prod if(conf.getString("prod.db.default.driver").isDefined) => "prod.db.default.driver"
       //default mode
       case _ => "db.default.driver"
     }
+    val driverConfValue = conf.getString(driverConfKey)
+    if (driverConfValue.isDefined) driverConfValue.get 
+    else throw conf.reportError("slick jdbc driver", "Slick error : jdbc driver not defined in application.conf for " + Play.application.mode + "mode", None)
   }
 }
