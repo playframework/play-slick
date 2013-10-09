5df39e7225a50c197b2017d81aedf50c30c25666
Fredrik Ekholdt <freekh@gmail.com>
2013-10-09 11:22:08 +0200

    updating examples to use classes and DAOs instead of objects

--- samples/play-slick-sample/app/models/Cat.scala
+++ samples/play-slick-sample/app/models/Cat.scala
@@ -4,7 +4,11 @@ import play.api.db.slick.Config.driver.simple._
 
 case class Cat(name: String, color: String)
 
-object Cats extends Table[Cat]("CAT") {
+/* Table mapping
+ * 
+ * NOTE: objects extending Table not be used
+ */
+class Cats extends Table[Cat]("CAT") {
 
   def name = column[String]("name", O.PrimaryKey)
   def color = column[String]("color", O.NotNull)
