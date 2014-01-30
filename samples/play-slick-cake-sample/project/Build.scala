189afd77a082ed7f1dba99b3a9bb2add1a38c652
Fredrik Ekholdt <freekh@gmail.com>
2014-01-30 10:03:53 +0100

    minor change: fixed name of project

--- samples/play-slick-cake-sample/project/Build.scala
+++ samples/play-slick-cake-sample/project/Build.scala
@@ -4,7 +4,7 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "play-slick-sample"
+  val appName         = "play-slick-cake-sample"
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
