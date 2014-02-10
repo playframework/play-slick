b080445e7d565c5b7bdfdca5779be9b01766c733
Marc Siegel <marc.siegel@timgroup.com>
2014-02-10 15:25:49 -0500

    Import play-slick-iteratee-sample from scalaquery-play-iteratees

---
+++ samples/play-slick-iteratee-sample/app/controllers/Application.scala
@@ -0,0 +1,38 @@
+package controllers
+
+import play.api.mvc._
+import play.api.libs.iteratee.Enumeratee
+import play.api.libs.json.Json.toJson
+import play.api.libs.json.Writes.traversableWrites
+import play.api.libs.Comet
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+import models._
+
+object Application extends Controller {
+  implicit val recordsWrites = traversableWrites(records.RecordWrites)
+
+  def index = Action { request =>
+    Ok(views.html.index())
+  }
+
+  def list = Action { request =>
+    records.ensureDbPopulated()
+    Ok(toJson(records.all))
+  }
+
+  def listComet = Action { request =>
+    records.ensureDbPopulated()
+
+    // Records fetched in chunks of 2, and asynchronously piped out to
+    // browser in chunked http responses, to be handled by comet callback.
+    //
+    // see http://www.playframework.com/documentation/2.2.x/ScalaComet
+    val pipeline = records.enumerateAllInChunksOfTwo &>
+      Enumeratee.map(toJson(_)) &>
+      Comet(callback = "parent.cometMessage")
+
+    Ok.chunked(pipeline)
+  }
+
+}
---
+++ samples/play-slick-iteratee-sample/app/models/Record.scala
@@ -0,0 +1,54 @@
+package models
+
+import play.api.db.DB
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.json.{JsObject, JsValue, Writes}
+import play.api.libs.json.Json._
+import play.api.Logger
+import play.api.Play.current
+
+import scala.slick.driver.H2Driver
+import scala.slick.driver.H2Driver.simple._
+
+import com.timgroup.scalaquery_play_iteratees.ScalaQueryPlayIteratees.{enumerateScalaQuery, PlayLogCallback}
+
+case class Record(id: Int, name: String)
+
+class Records(tag: Tag) extends Table[Record](tag, "records") {
+  def id   = column[Int   ]("id")
+  def name = column[String]("name")
+  def * = (id, name) <> (Record.tupled, Record.unapply)
+}
+
+object records extends TableQuery(new Records(_)) {
+  def database = Database.forDataSource(DB.getDataSource("default"))
+  lazy val profile = H2Driver
+
+  def mkQuery = for { r <- this } yield r
+
+  def count = database withSession { implicit s => mkQuery.length.run }
+
+  def all = database withSession { implicit s => mkQuery.list }
+
+  /** This is it: enumerate the query for all Records in chunks of 2 */
+  def enumerateAllInChunksOfTwo = enumerateScalaQuery(profile, Right(database), mkQuery, maybeChunkSize = Some(2),
+    logCallback = PlayLogCallback(Logger /*, shouldLogSqlOnSuccess = true */ )) // uncomment to log SQL on successful fetches
+
+  def ensureDbPopulated() {
+    if (count == 0) {
+      val records = Seq(
+        Record(1, "Alpha"),
+        Record(2, "Beta"),
+        Record(3, "Gamma"),
+        Record(4, "Delta"),
+        Record(5, "Epsilon"))
+      database withSession { implicit s => this.insertAll(records:_*) }
+    }
+  }
+
+  // serialize Record to json
+  implicit object RecordWrites extends Writes[Record] {
+    def writes(r: Record): JsValue = JsObject(Seq("id" -> toJson(r.id), "name" -> toJson(r.name)))
+  }
+
+}
