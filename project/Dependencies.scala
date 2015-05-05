9f3f55f2bb9e20eb8253fe315a5313d190c173d7
Mirco Dotta <mirco.dotta@gmail.com>
2015-05-05 13:02:41 +0200

    Cleaned up project's dependencies
    Lots of dependencies are not used, hence they are removed.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,11 +5,7 @@ object Dependencies {
   val playSlick = Seq(
     Library.playJdbc,
     Library.slick,
-    Library.javaxServlet,
-    Library.findbugs,
-    Library.playSpecs2 % "test",
-    Library.hsqldb % "test",
-    Library.mockito % "test"
+    Library.playSpecs2 % "test"
   )
 
   val resolvers = DefaultOptions.resolvers(snapshot = true) ++ Seq(
@@ -21,18 +17,10 @@ object Dependencies {
 object Version {
   val play = _root_.play.core.PlayVersion.current
 
-  val findbugs     = "2.0.1"
-  val hsqldb       = "2.3.1"
-  val javaxServlet = "3.0.1"
-  val mockito      = "1.9.5"
   val slick        = "3.0.0"
 }
 
 object Library {
-  val findbugs     = "com.google.code.findbugs" %  "jsr305"            % Version.findbugs
-  val hsqldb       = "org.hsqldb"               %  "hsqldb"            % Version.hsqldb
-  val javaxServlet = "javax.servlet"            %  "javax.servlet-api" % Version.javaxServlet
-  val mockito      = "org.mockito"              %  "mockito-all"       % Version.mockito
   val playJdbc     = "com.typesafe.play"        %% "play-jdbc"         % Version.play
   val playSpecs2   = "com.typesafe.play"        %% "play-specs2"       % Version.play
   val slick        = "com.typesafe.slick"       %% "slick"             % Version.slick
