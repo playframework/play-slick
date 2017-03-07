78fd002afc5bb2de6a18f00df14efcc15f9a6b34
Jose C <cardona.josem@gmail.com>
2017-03-07 13:54:00 -0500

    Updated documentation for play-slick usage (#387)
    * Updated the DI code to current practices
    
    * Updated documentation.
    
    Removed reference to deprecated Play.current to reflect current Play-slick 2.0.x best practices
    
    * Updated global lookup file
    
    Since the reference to Play.current is now deprecated, I changed the objects to classes
    
    * Fixed some import statments
    
    * Fixed import statement for JdbcProfile
    
    It was moved from slick.jdbc to slick.driver in 3.1
    
    * Changed from provider to direct reference

--- docs/manual/working/scalaGuide/main/sql/slick/code/DI.scala
+++ docs/manual/working/scalaGuide/main/sql/slick/code/DI.scala
@@ -9,28 +9,27 @@ import javax.inject.Inject
 import scala.concurrent.Future
 
 import play.api.mvc._
-import play.api.db.slick.DatabaseConfigProvider
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
-import slick.jdbc.JdbcProfile
+import slick.driver.JdbcProfile
 
 import UsersSchema._
 
 //#di-database-config
-class Application @Inject() (dbConfigProvider: DatabaseConfigProvider) extends Controller {
-  val dbConfig = dbConfigProvider.get[JdbcProfile]
+class Application @Inject() (protected val dbConfigProvider: DatabaseConfigProvider) extends Controller with HasDatabaseConfigProvider[JdbcProfile] {
   //#di-database-config
 
-  import dbConfig.profile.api._
+  import driver.api._
 
   def index(name: String) = Action.async { implicit request =>
-    val resultingUsers: Future[Seq[User]] = dbConfig.db.run(Users.filter(_.name === name).result)
+    val resultingUsers: Future[Seq[User]] = db.run(Users.filter(_.name === name).result)
     resultingUsers.map(users => Ok(views.html.index(users)))
   }
 }
 
 import play.db.NamedDatabase
 //#named-di-database-config
-class Application2 @Inject() (@NamedDatabase("<db-name>") dbConfigProvider: DatabaseConfigProvider) extends Controller {
+class Application2 @Inject() (@NamedDatabase("<db-name>") protected val dbConfigProvider: DatabaseConfigProvider) extends Controller with HasDatabaseConfigProvider[JdbcProfile] {
   //#named-di-database-config
 }
