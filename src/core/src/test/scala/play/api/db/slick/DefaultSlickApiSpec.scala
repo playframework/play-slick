2f3522cfe7cff195e797571c5870f8790af4f10a
Renato  Cavalcanti <renato@cavalcanti.be>
2018-11-14 14:32:30 +0100

    make test robust with respect to any change in play

--- src/core/src/test/scala/play/api/db/slick/DefaultSlickApiSpec.scala
+++ src/core/src/test/scala/play/api/db/slick/DefaultSlickApiSpec.scala
@@ -24,31 +24,36 @@ class DefaultSlickApiSpec extends Specification with Mockito { self =>
   }
 
   "DefaultSlickApi" should {
+
     "check the assumption that the ApplicationLifecycle isn't null" in {
       val lifecycle = injector.instanceOf[ApplicationLifecycle]
       lifecycle must not(beNull)
     }
+
     "check the assumption that the ApplicationLifecycle is a singleton" in {
       val injector = self.injector
       val lifecycle1 = injector.instanceOf[ApplicationLifecycle]
       val lifecycle2 = injector.instanceOf[ApplicationLifecycle]
       lifecycle1 mustEqual lifecycle2
     }
+
     "check the assumption that ApplicationLifecycle is binded to a DefaultApplicationLifecycle" in {
       val lifecycle = injector.instanceOf[ApplicationLifecycle]
       lifecycle must beAnInstanceOf[DefaultApplicationLifecycle]
     }
-    "check that no stop hook is registered in the ApplicationLifecycle at application start" in {
-      val lifecycle = injector.instanceOf[ApplicationLifecycle].asInstanceOf[DefaultApplicationLifecycle]
-      hooks(lifecycle) must have size (0)
-    }
+
     "check that a stop hook is registered in the ApplicationLifecycle when a database is created" in {
       val injector = self.injector
       val lifecycle = injector.instanceOf[ApplicationLifecycle].asInstanceOf[DefaultApplicationLifecycle]
+
+      // collect the num of existing hooks added by Play
+      val originalNumOfHooks = hooks(lifecycle).length
+
       val api = injector.instanceOf[SlickApi]
       api.dbConfig[BasicProfile](DbName("default"))
 
-      hooks(lifecycle) must have size (1)
+      // confirm that SlickApi is adding its own hook
+      hooks(lifecycle) must have size (originalNumOfHooks + 1)
     }
   }
 }
