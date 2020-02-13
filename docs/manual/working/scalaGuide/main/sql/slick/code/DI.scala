cd9c69800812ab9bad524715538503971035e6b2
Will Sargent <will.sargent@gmail.com>
2020-02-13 08:47:48 -0800

    Update the docs and formatting
    (cherry picked from commit 0cb4db1767555378a7c48d10762f7426a394d4f6)

--- docs/manual/working/scalaGuide/main/sql/slick/code/DI.scala
+++ docs/manual/working/scalaGuide/main/sql/slick/code/DI.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2015 Typesafe Inc. <http://www.typesafe.com>
+ * Copyright (C) 2011-2019 Lightbend Inc. <https://www.lightbend.com>
  */
 package scalaguide.slick
 package di
