369130421cd7f0bda76a8270ae92cec351a49e5b
Matthias Kurz <m.kurz@irregular.at>
2025-01-17 20:34:59 +0100

    Revert "Reformat with scalafmt 3.8.3"
    This reverts commit 457f3ff1067c995325df4b293ce52d3bf2a8a18c.

--- docs/manual/working/scalaGuide/main/sql/slick/code/DI.scala
+++ docs/manual/working/scalaGuide/main/sql/slick/code/DI.scala
@@ -21,6 +21,7 @@ class Application @Inject() (protected val dbConfigProvider: DatabaseConfigProvi
 ) extends AbstractController(cc)
     with HasDatabaseConfigProvider[JdbcProfile] {
   // #di-database-config
+
   import profile.api._
 
   def index(name: String) = Action.async { implicit request =>
